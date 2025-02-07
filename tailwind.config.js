5190bef4dfe8eed5068929ccf65b2c7c0e00683a
Anucha.tk <anuchataowkaen@gmail.com>
2025-02-07 18:15:55 +0700

    chore(font): add Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
