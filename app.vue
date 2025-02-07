351dd4a75a221d02c26720808d848ef73f7b00d2
Anucha.tk <anuchataowkaen@gmail.com>
2025-02-07 17:49:41 +0700

    chore(css): add tailwindcss and shadcn-vue

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="bg-blue-700">test</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 })
