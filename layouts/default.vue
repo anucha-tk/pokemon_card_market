8745aa3c8588a995e5cce54d141597e955a20daf
Anucha.tk <anuchataowkaen@gmail.com>
2025-02-08 09:53:48 +0700

    fix(layout): fix layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,8 @@
 <template>
-  <LayoutsPageHeader />
+  <div class="grid grid-flow-row grid-cols-1 gap-12 px-[123px] md:gap-16 lg:gap-20">
+    <div id="container" class="flex flex-col space-y-6 px-[29px] pt-6">
+      <LayoutsPageHeader />
+      <slot />
+    </div>
+  </div>
 </template>
