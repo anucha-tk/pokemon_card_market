0452a81e35ec77de1a16613c4067a2d515f93653
Anucha.tk <anuchataowkaen@gmail.com>
2025-02-10 17:54:47 +0700

    feat(cart button): disable button when no price

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import { ShoppingBag } from 'lucide-vue-next';
+  import clsx from 'clsx';
+
   const { p } = defineProps<{
     p: Pokemon;
   }>();
@@ -8,7 +10,7 @@
 </script>
 
 <template>
-  <div id="card" class="flex w-full flex-col items-center self-stretch">
+  <div id="card" class="flex w-full flex-col items-center self-stretch md:max-w-[176px]">
     <!-- Image Section (On Top) -->
     <section id="poke_img" class="relative flex w-full justify-center">
       <NuxtImg
@@ -19,20 +21,30 @@
 
     <!-- Background Card Section -->
     <div
-      class="relative -mt-10 flex h-full w-full flex-col justify-between rounded-lg bg-[#1F1D2B] p-4 text-center"
+      class="relative -mt-10 flex h-full w-full flex-col justify-between rounded-lg bg-slate-200 p-4 text-center dark:bg-[#1F1D2B]"
     >
-      <p class="pt-10 font-semibold text-white">{{ p.name }}</p>
-      <p class="text-sm font-normal text-gray-400">$ {{ priceMarket }} • {{ cardTotal }} Cards</p>
+      <p class="pt-10 font-semibold dark:text-white">{{ p.name }}</p>
+      <p class="text-sm font-normal dark:text-gray-400">
+        $ {{ priceMarket }} • {{ cardTotal }} Cards
+      </p>
       <!-- Add to Cart Button -->
       <button
         :disabled="priceMarket === 'N/A'"
-        class="mt-4 flex w-full items-center justify-center gap-2 rounded-lg bg-[#312F3C] py-2 text-white hover:bg-[#474651] disabled:text-[#83828A] disabled:hover:bg-[#312F3C]"
+        :class="
+          clsx(
+            'mt-4 flex w-full items-center justify-center gap-2 rounded-lg bg-primary py-2 text-white hover:bg-gray-700 dark:bg-[#312f3c] dark:hover:bg-[#474651]',
+            priceMarket === 'N/A' &&
+              'pointer-events-none hover:bg-[#ABBBC2] dark:hover:bg-[#312f3c]'
+          )
+        "
       >
         <ShoppingBag
           style="width: 14px; height: 14px"
           :class="priceMarket === 'N/A' ? 'text-[#82838B]' : 'text-white'"
         />
-        <span class="text-sm font-medium">Add to cart</span>
+        <span :class="clsx('text-sm font-medium', priceMarket === 'N/A' && 'text-[#82838B]')"
+          >Add to cart</span
+        >
       </button>
     </div>
   </div>
